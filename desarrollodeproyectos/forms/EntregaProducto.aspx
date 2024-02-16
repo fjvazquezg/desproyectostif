f0eba474bc7fd24a659bd6a700c14d40868eb696
YamirSitoC <castroyamir123@gmail.com>
2024-02-16 12:05:15 -0700

    Frontend entrega de producto

---
+++ desarrollodeproyectos/forms/EntregaProducto.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="EntregaProducto.aspx.cs"
+    Inherits="desarrollodeproyectos.About" %>
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Inicio sin contraseña</title>
+        <link rel="stylesheet" href="bootstrap.min.css">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="/desarrollodeproyectos/Scripts/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="/desarrollodeproyectos/forms/EntregaDeProducto.css">
+    </head>
+
+    <body>
+
+        <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+            <div class="d-flex justify-content-between align-items-center">
+
+                <a href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-left" width="40"
+                        height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none"
+                        stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M5 12l14 0" />
+                        <path d="M5 12l6 6" />
+                        <path d="M5 12l6 -6" />
+                    </svg>
+                </a>
+                <h1 class="navbar-brand">ENTREGA DE PRODUCTO</h1>
+            </div>
+              
+        </nav>
+        <div class="container">
+            <div class="left-section">
+                <!-- Sección izquierda -->
+                <h1>ENTREGA DE PRODUCTO</h1>
+                <p class="lead" value="NOMBRE">Yamir Castro Quiroz</p>
+                <p class="lead" value="CONTACTO">687-165-21-03</p>
+            </div>
+            <div class="right-section">
+                <!-- Sección derecha -->
+                <h2>ENVIAR CODIGO DE VERIFICACION</h2>
+                <p>Enviaremos un código al correo del usuario</p>
+                <div class="qr-section">
+                    <button class="btn btn-primary">Código QR</button>
+                    <div id="qr-code-display" class="mb-3 qr-code-box">
+                        <!-- Aquí se mostrará el código QR -->
+                    </div>
+                </div>
+                <div class="sms-section">
+                    <button class="btn btn-secondary">CLAVE</button>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" maxlength="5">
+                    </div>
+                </div>
+                <button class="btn btn-success">Enviar y Confirmar</button>
+            </div>
+        </div>
+    </body>
+
+    </html>
---
+++ desarrollodeproyectos/forms/EntregaProducto.aspx.designer.cs
@@ -0,0 +1,206 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace desarrollodeproyectos
+{
+
+
+    public partial class About
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control EmailTextBox.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox EmailTextBox;
+
+        /// <summary>
+        /// Control ChangeAccountButton.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ChangeAccountButton;
+
+        /// <summary>
+        /// Control QRButton.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink QRButton;
+
+        /// <summary>
+        /// Control SMSButton.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink SMSButton;
+
+        /// <summary>
+        /// Control CodeInput1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox CodeInput1;
+
+        /// <summary>
+        /// Control CodeInput2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox CodeInput2;
+
+        /// <summary>
+        /// Control CodeInput3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox CodeInput3;
+
+        /// <summary>
+        /// Control CodeInput4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox CodeInput4;
+
+        /// <summary>
+        /// Control CodeInput5.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox CodeInput5;
+
+        /// <summary>
+        /// Control CodeInput6.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox CodeInput6;
+
+        /// <summary>
+        /// Control ConfirmButton.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ConfirmButton;
+
+        /// <summary>
+        /// Control QRModalCloseButton.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink QRModalCloseButton;
+
+        /// <summary>
+        /// Control SMSModalCloseButton.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink SMSModalCloseButton;
+
+        /// <summary>
+        /// Control SMSCodeInput1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox SMSCodeInput1;
+
+        /// <summary>
+        /// Control SMSCodeInput2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox SMSCodeInput2;
+
+        /// <summary>
+        /// Control SMSCodeInput3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox SMSCodeInput3;
+
+        /// <summary>
+        /// Control SMSCodeInput4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox SMSCodeInput4;
+
+        /// <summary>
+        /// Control SMSCodeInput5.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox SMSCodeInput5;
+
+        /// <summary>
+        /// Control SMSCodeInput6.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox SMSCodeInput6;
+
+        /// <summary>
+        /// Control SMSConfirmButton.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button SMSConfirmButton;
+    }
+}
