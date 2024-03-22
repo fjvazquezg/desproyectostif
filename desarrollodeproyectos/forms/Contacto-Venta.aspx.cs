f32b034721693b9cbaee2ac605797c8d0fada744
Pavel Contreras Araux <72897346+JPavelCA@users.noreply.github.com>
2024-03-22 09:09:57 -0700

    funcionamiento de boton whatsapp

---
+++ desarrollodeproyectos/forms/Contacto-Venta.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace desarrollodeproyectos.forms
+{
+    public partial class Contacto_Venta : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnChat_Click(object sender, EventArgs e)
+        {
+            // Redirigir al chat privado
+            Response.Redirect("Chat_Privado.html");
+        }
+
+        protected void btnWhatsApp_Click(object sender, EventArgs e)
+        {
+            // Redirigir al contacto de WhatsApp
+            Response.Redirect("https://api.whatsapp.com/send?phone=526871001414");
+        }
+    }
+}
