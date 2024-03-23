e52769dd94eed0c61806282e087c751c18a235a0
Citlali <citlali6soto7@gmail.com>
2024-03-23 02:01:29 -0700

    Se agregó las consultas al formulario de Métodos de comprobación

---
+++ desarrollodeproyectos/forms/MetodosDeComprobacion.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MetodosDeComprobacion.aspx.cs" Inherits="desarrollodeproyectos.forms.MetodosDeComprobacion" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Verificación de documentos</title>
+    <link href="/css/bootstrap.min.css" rel="stylesheet" />
+</head>
+
+<body>
+    <form id="form1" runat="server" enctype="multipart/form-data">
+        <div class="container mt-5">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <h2 class="mb-4 text-center">VERIFICACIÓN DE DOCUMENTOS</h2>
+                   
+                    <div class="form-group">
+                        <label for="nombreCompleto">Nombre Completo:</label>
+                        <asp:TextBox ID="nombreCompleto" runat="server" CssClass="form-control" required ></asp:TextBox>
+                    </div>
+                
+                         <div class="form-group">
+                    <label for="fileUploadControl">Seleccionar imagen:</label>
+                    <input type="file" id="fileUploadControl" runat="server" class="form-control" />
+                       </div>
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <asp:Button ID="btnEnviar" runat="server" Text="Enviar" CssClass="btn btn-primary mt-4 btn-lg btn-block" />
+                        </div>
+                    </div>
+                    <asp:Label ID="lblMensaje" runat="server" CssClass="mt-3" ForeColor="Red"></asp:Label>
+                </div>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ desarrollodeproyectos/forms/MetodosDeComprobacion.aspx.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+namespace desarrollodeproyectos.forms
+{
+    public partial class MetodosDeComprobacion : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CargarDatosUsuario();
+            }
+        }
+
+        protected void CargarDatosUsuario()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["connDB"].ConnectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = conn;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = "SP_USUARIO";
+
+                    cmd.Parameters.AddWithValue("@OP", 1);
+                    cmd.Parameters.AddWithValue("@USU_ID", 0);
+                    cmd.Parameters.AddWithValue("@USU_NOMBRE", "NombreNuevo");
+                    cmd.Parameters.AddWithValue("@USU_IMG", "ruta_de_la_imagen");
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                lblMensaje.Text = "Error al cargar los datos del usuario: " + ex.Message;
+            }
+        }
+
+        protected void btnEnviar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int idUsuario;
+                if (int.TryParse(nombreCompleto.Text, out idUsuario))
+                {
+                    // string rutaImagen = ObtenerRutaDeLaImagen();
+
+                    using (SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["connDB"].ConnectionString))
+                    {
+                        conn.Open();
+
+                        SqlCommand cmd = new SqlCommand();
+                        cmd.Connection = conn;
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandText = "SP_USUARIO";
+
+                        cmd.Parameters.AddWithValue("@OP", 2);
+                        cmd.Parameters.AddWithValue("@USU_ID", idUsuario);
+                        //cmd.Parameters.AddWithValue("@USU_IMG", rutaImagen); 
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                else
+                {
+                    lblMensaje.Text = "Por favor, ingresa un ID de usuario válido.";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblMensaje.Text = "Error al guardar los datos: " + ex.Message;
+            }
+        }
+
+        // Método para obtener la ruta de la imagen 
+        /*private string ObtenerRutaDeLaImagen()
+        {
+            if (fileUploadControl.HasFile)
+            {
+                string fileName = fileUploadControl.FileName;
+                string filePath = "~/images/" + fileName; // Ruta donde se guardará la imagen
+                fileUploadControl.SaveAs(Server.MapPath(filePath));
+                return filePath;
+            }
+            else
+            {
+                return "ruta_por_defecto"; // Ruta por defecto si no se selecciona ninguna imagen
+            }
+        }*/
+    }
+}
